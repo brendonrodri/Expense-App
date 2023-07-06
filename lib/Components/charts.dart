a5b77232d156208f3d825fc8e43827e48efec326
brendonrodri <rodriguesbrendon861@gmail.com>
2023-07-06 11:56:46 -0300

    feat: componente Chart criado

---
+++ lib/Components/charts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      elevation: 5,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
